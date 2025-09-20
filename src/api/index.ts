b127297cd02244ee219b0188bbf3ed7f43c6268f
Choo Ern Tuong <115686447+cetuong@users.noreply.github.com>
2025-09-20 16:12:57 +0800

    clean up

--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./postSignup";
-export * from "./postLogin";
-export * from "./getAboutUs";
-export * from "./postEmailSubscribe";
-export * from "./getEmailSubscribe";
-export * from "./postContactUs";
